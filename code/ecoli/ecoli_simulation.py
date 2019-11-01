937d8a4fa46e0d5a65167282a16d088351a8af26
SwastikRug <swastiknayak23@gmail.com>
2019-11-01 19:44:24 +0100

    Adding configs

--- code/ecoli/ecoli_simulation.py
+++ code/ecoli/ecoli_simulation.py
@@ -19,15 +19,6 @@ class Ecoli:
     def ecoli_count(self):
         return self.ecolicount
 
-    # @property
-    # def getBetaGalctosidase(self):
-    #     return self.betaGalactosidase
-    #
-    # @property
-    # def getStarvationPeriod(self):
-    #     return self.starvationperiod
-
-
 class Glucose:
     def __init__(self, count):
         self.glucosecount = count
@@ -64,6 +55,15 @@ class LactaseEnzyme:
         return self.lactase
 
 
+class LactaseSwapPeriod:
+    def __init__(self, count):
+        self.lactaseSwap = count
+
+    @property
+    def lactase_swap_period(self):
+        return self.lactaseSwap
+
+
 parser = ConfigParser()
 parser.read("conf.ini")
 
@@ -74,8 +74,10 @@ Radiobutton(master, text="Normal", variable=var, value='Normal').grid(row=1, sti
 Radiobutton(master, text="OnlyLactose", variable=var, value='OnlyLactose').grid(row=2, sticky=W)
 Radiobutton(master, text="Starvation", variable=var, value='Starvation').grid(row=3, sticky=W)
 Radiobutton(master, text="stable_colony", variable=var, value='stable_colony').grid(row=4, sticky=W)
-Radiobutton(master, text="custom", variable=var, value='custom').grid(row=5, sticky=W)
-Button(master, text="OK", command=master.quit).grid(row=6, sticky=W)
+Radiobutton(master, text="stable_colony_glucose", variable=var, value='stable_colony_glucose').grid(row=5, sticky=W)
+Radiobutton(master, text="stable_colony_lactose", variable=var, value='stable_colony_lactose').grid(row=6, sticky=W)
+Radiobutton(master, text="custom", variable=var, value='custom').grid(row=7, sticky=W)
+Button(master, text="OK", command=master.quit).grid(row=8, sticky=W)
 mainloop()
 
 
@@ -105,38 +107,17 @@ glucose_count = Glucose(parser.getint(selection, "glucose_count"))
 galactose_count = Galactose(parser.getint(selection, "galactose_count"))
 lactose_count = Lactose(parser.getint(selection, "lactose_count"))
 beta_galactosidase = LactaseEnzyme(parser.getint(selection, "lactase"))
-replenish_glucose = parser.getint(selection, "replenish_glucose") if (parser.has_option(selection, "replenish_glucose")) else 0
-replenish_galactose = parser.getint(selection, "replenish_galactose") if (parser.has_option(selection, "replenish_galactose")) else 0
-replenish_lactose = parser.getint(selection, "replenish_lactose") if (parser.has_option(selection, "replenish_lactose")) else 0
-replenish_interval = parser.getint(selection, "replenish_interval") if (parser.has_option(selection, "replenish_interval")) else 0
+replenish_glucose = parser.getint(selection, "replenish_glucose") if (
+    parser.has_option(selection, "replenish_glucose")) else 0
+replenish_galactose = parser.getint(selection, "replenish_galactose") if (
+    parser.has_option(selection, "replenish_galactose")) else 0
+replenish_lactose = parser.getint(selection, "replenish_lactose") if (
+    parser.has_option(selection, "replenish_lactose")) else 0
+replenish_interval = parser.getint(selection, "replenish_interval") if (
+    parser.has_option(selection, "replenish_interval")) else 0
 sample_frame = 2
+lactose_swap_period = LactaseSwapPeriod(0)
 
-
-# window = Tk()
-# User_input = Entry()
-# User_input.pack()
-# #\user_problem  = int(User_input.get())
-# window.mainloop()
-#
-# user_problem  = int(User_input.get())
-# print(user_problem)
-
-# itr = 50
-# seed_input = [500, 1000]
-# time = np.linspace(0, itr + 1, itr + 1)
-#
-# ecoli_count = Ecoli(1)
-# glucose_count = Glucose(100000)
-# galactose_count = Galactose(1000)
-# lactose_count = Lactose(100000)
-# beta_galactosidase = LactaseEnzyme(10000)
-# sample_frame = 2
-
-
-# ecoli_colony = []
-
-# for i in range(sample_frame):
-#    ecoli_colony.append(ecoli_count)
 
 def create_seed_array(initial_seed, dummy, frame, extra=1):
     return np.concatenate([np.full(frame, initial_seed), np.full(itr + extra - sample_frame, dummy)], axis=0)
@@ -192,9 +173,6 @@ def calc_possible_lactose_metabolism(cellColony, lactoseAvailable):
 
 def multiply_under_adundant_resources(expected_resource_requirement,
                                       expected_ecoli_division,
-                                      curr_ecoli_count,
-                                      lactose_swap_period,
-                                      possible_lactose_metabolism,
                                       itr):
     glucose_consumed = glucose_count.glucose_count - expected_resource_requirement
     deduct_from_galactose = (galactose_count.galactose_count - glucose_consumed) if glucose_consumed < 0 else 0
@@ -223,10 +201,13 @@ starvationperiod = 0
 
 
 def consume_resources_and_multiply(itr):
-    lactose_swap_period = 0
     colony_status = check_if_colony_is_dead(itr)
     if colony_status:
         print("The E-coli colony is dead")
+        dldt[itr] = lactose_count.lactose_count
+        dgdt[itr] = glucose_count.glucose_count
+        dgadt[itr] = galactose_count.galactose_count
+        lactaseDt[itr] = beta_galactosidase.lactase
     #    elif resource_scarcity <= 0:
     #        lactose_availability = check_for_lactose_availability(itr)
     #        partial_multiplication_and_starvation(itr)
@@ -244,27 +225,29 @@ def consume_resources_and_multiply(itr):
         total_resources_available = available_glucose_galactose + possible_lactose_metabolism
         insufficient_raw_resources = True if available_glucose_galactose - expected_resource_requirement < 0 else False
         resource_state = total_resources_available - expected_resource_requirement
-        # curr_lactase_count = sum(l for l in (lactaseDt[itr - 20:itr] if itr >= 20 else lactaseDt[0:itr + 1]))
         lactaseDt[itr] = beta_galactosidase.lactase
 
-        lactose_swap_period = 0 if insufficient_raw_resources else lactose_swap_period + 1
-        if lactose_swap_period > 2:
+        if lactose_swap_period.lactase_swap_period > 2:
             glucose_count.glucosecount += possible_lactose_metabolism
             galactose_count.galactosecount += possible_lactose_metabolism
             lactose_count.lactosecount -= possible_lactose_metabolism
-            beta_galactosidase.lactase = beta_galactosidase.lactase + ecoli_count.ecoli_count * 50
+            beta_galactosidase.lactase = (beta_galactosidase.lactase + ecoli_count.ecoli_count * 50) if(lactose_count.lactose_count > 0) else beta_galactosidase.lactase
             lactaseDt[itr] = beta_galactosidase.lactase
 
+        # if Lactose is the only source, decrease the growth by 20%
+        expected_ecoli_division -= 0 if available_glucose_galactose > 0 else expected_ecoli_division * 0.20
+
         if resource_state >= 0:
+            if available_glucose_galactose >= expected_resource_requirement and lactose_swap_period.lactase_swap_period > 0:
+                lactose_swap_period.lactaseSwap = lactose_swap_period.lactase_swap_period - 1
+            else:
+                lactose_swap_period.lactaseSwap = lactose_swap_period.lactase_swap_period + 1
 
             multiply_under_adundant_resources(expected_resource_requirement,
                                               expected_ecoli_division,
-                                              curr_ecoli_count,
-                                              lactose_swap_period,
-                                              possible_lactose_metabolism,
                                               itr)
 
-            # Colony starvation - Assuming colony survives for 2 iterations and kills 30% of the population
+        # Colony starvation - Assuming colony survives for 2 iterations and kills 30% of the population
         # elif sufficient_raw_resources <= 0 < possible_lactose_metabolism:
 
         elif resource_state <= 0:
@@ -275,13 +258,11 @@ def consume_resources_and_multiply(itr):
             cells_with_sufficient_resources = expected_ecoli_division - cells_under_starvation
             # bug
             dedt[itr] = ecoli_count.ecoli_count + cells_with_sufficient_resources
-            dedt[itr] = (dedt[itr] - dedt[itr] * 0.3)  # if (starvationperiod > 2) else dedt[itr]
-            # dedt[itr].starvationperiod += 1 if (dedt[itr].starvationperiod > 2) else -dedt[itr].starvationperiod
-            # starvationperiod += -starvationperiod if (starvationperiod > 2) else 1
+            dedt[itr] = (dedt[itr] - dedt[itr] * 0.3)
             ecoli_count.ecolicount = dedt[itr]
-            dgdt[itr] = 0  # glucose_count.glucose_count
+            dgdt[itr] = 0
             glucose_count.glucosecount = 0
-            dgadt[itr] = 0  # galactose_count.galactose_count
+            dgadt[itr] = 0
             galactose_count.galactosecount = 0
             lactose_count.lactosecount = dldt[itr] = lactose_count.lactose_count - possible_lactose_metabolism
             lactaseDt[itr] = beta_galactosidase.lactase
@@ -291,6 +272,8 @@ def consume_resources_and_multiply(itr):
             dgadt[itr] = galactose_count.galactose_count
             dldt[itr] = lactose_count.lactose_count
             lactaseDt[itr] = beta_galactosidase.lactase
+
+        return lactose_swap_period
 
 
 for i in range(itr + 1):
@@ -303,29 +286,45 @@ for i in range(itr + 1):
 
 style.use("fivethirtyeight")
 fig = plot.figure("Ecoli bacterial growth simulation:" + selection)
-ax = fig.add_subplot(1, 1, 1)
-
+ax = fig.add_subplot(2, 2, 1)
+ax2 = fig.add_subplot(2, 2, 2)
+ax3 = fig.add_subplot(2, 2, 3)
+ax4 = fig.add_subplot(2, 2, 4)
 
 def displayPlot(i):
     #    ax1.clear()
     ax.cla()
+    ax2.cla()
+    ax3.cla()
+    ax4.cla()
     ax.title.set_text('Ecoli bacterial growth simulation:' + selection)
-    if parser.getboolean("display_control", "plot_glucose"):
-        ax.plot(time[0:i], dgdt[0:i], 'r', label='Glucose-Consumption')
     if parser.getboolean("display_control", "plot_ecoli"):
         ax.plot(time[0:i], dedt[0:i], 'g', label='Ecoli-Multiplication')
+    if parser.getboolean("display_control", "plot_glucose"):
+        ax2.plot(time[0:i], dgdt[0:i], 'r', label='Glucose-Consumption')
     if parser.getboolean("display_control", "plot_galactose"):
-        ax.plot(time[0:i], dgadt[0:i], 'y', label='GalactoseConsumption')
+        ax2.plot(time[0:i], dgadt[0:i], 'y', linestyle="-.", label='GalactoseConsumption')
     if parser.getboolean("display_control", "plot_lactase"):
-        ax.plot(time[0:i], lactaseDt[0:i], 'b', label='Lactase enzyme')
+        ax3.plot(time[0:i], lactaseDt[0:i], 'b', label='Lactase enzyme')
+    if parser.getboolean("display_control", "plot_lactose"):
+        ax4.plot(time[0:i], dldt[0:i], 'c', linestyle="--", label='Lactose Consumption')
     ax.legend()
     ax.set_xlabel('Time in min')
     ax.set_ylabel('Ecoli Growth')
+    ax2.legend()
+    ax2.set_xlabel('Time in min')
+    ax2.set_ylabel('Glucose + Galactose')
+    ax3.legend()
+    ax3.set_xlabel('Time in min')
+    ax3.set_ylabel('Lactase enzyme')
+    ax4.legend()
+    ax4.set_xlabel('Time in min')
+    ax4.set_ylabel('Lactose Consumption')
 
 
 #  ax1.plot(time[0:i], dldt[0:i], 'c', label='LactoseConsumption')
 
 
-ani = animation.FuncAnimation(fig, displayPlot, interval=500)
+ani = animation.FuncAnimation(fig, displayPlot, interval=100)
 plot.tight_layout()
 plot.show()
